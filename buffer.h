95072367425f02b14f37849a8ccd1bde29fc69ad
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-03-18 22:26:19 -0500

    Create buffer.h

---
+++ buffer.h
@@ -0,0 +1,11 @@
+/******************************* 
+ * Producer & Consumer problem *
+ *  -Solved using Pthreads     *
+ *  -Holds buffer definition   *
+ * Author: Philip Wright       *
+ * Date: 3/1/2021              *
+ *******************************/
+
+/* buffer.h */
+typedef int buffer_item; 
+#define BUFFER_SIZE 5
